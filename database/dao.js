d0a548b760159af95bcefa39523cdff60e444e18
Thiago Lins <tjlh@discente.ifpe.edu.br>
2022-08-17 20:19:16 -0300

    Fazendo ultimas alterações - pc do IF

--- database/dao.js
+++ database/dao.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password:'ellenerafa',
+    password:'ifpe1234',
     database: 'login',
     waitForConnections: true,
     connectionLimit: 10,
